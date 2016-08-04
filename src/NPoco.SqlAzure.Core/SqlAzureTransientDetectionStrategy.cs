dc3475a2765eb134450b536df597afff304288eb
David Savage <David Savage>
2016-08-03 21:34:41 -0500

    Added initial support for .net core
    Add builds and project updates for AppVeyor
    
    config fix
    
    ci-update (#1)
    
    * Add missing script stuff
    
    * Invoke build on SLN
    
    * well duh, get the path right dummy
    
    * Remove classic  project
    
    repo tag
    
    Revert "repo tag"
    
    This reverts commit 956b2ee88b796071ffa1bd77e9fa18bf2895b9a5.

---
+++ src/NPoco.SqlAzure.Core/SqlAzureTransientDetectionStrategy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+
+namespace NPoco.SqlAzure
+{
+
+    public sealed class SqlAzureTransientDetectionStrategy : ITransientErrorDetectionStrategy
+    {
+        public bool IsTransient(Exception ex)
+        {
+            if (ex != null)
+            {
+                SqlException sqlException;
+                if ((sqlException = ex as SqlException) != null)
+                {
+                    foreach (SqlError error in sqlException.Errors)
+                    {
+                        switch (error.Number)
+                        {
+                            case 40501:
+                                ThrottlingCondition throttlingCondition = ThrottlingCondition.FromError(error);
+                                sqlException.Data[(object)throttlingCondition.ThrottlingMode.GetType().Name] = (object)throttlingCondition.ThrottlingMode.ToString();
+                                sqlException.Data[(object)throttlingCondition.GetType().Name] = (object)throttlingCondition;
+                                return true;
+                            case 40540:
+                            case 40613:
+                            case 10928:
+                            case 10929:
+                            case 40143:
+                            case 40197:
+                            case 233:
+                            case 10053:
+                            case 10054:
+                            case 10060:
+                            case 20:
+                            case 64:
+                                return true;
+                            default:
+                                continue;
+                        }
+                    }
+                }
+                else
+                {
+                    return ex is TimeoutException;
+                }
+            }
+            return false;
+        }
+    }
+}
